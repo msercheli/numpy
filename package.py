4dbf45db245af42eec6c97e3e342bd2af1165dfc
Marcelo Sercheli <msercheli@gmail.com>
2019-04-03 10:19:51 -0400

    wip. need to include the resulting properly to be used in python

--- package.py
+++ package.py
@@ -13,10 +13,11 @@ build_requires = [
     "python-2.7"
 ]
 
-build_command = "py -m pip install {env.REZ_REPO_PAYLOAD_DIR}/numpy-{this.version}-cp27-cp27mu-manylinux1_x86_64.whl --prefix={root}"
+build_command = "pip install /home/marcelo/Dev/payload/numpy/numpy-1.16.2-cp27-cp27mu-manylinux1_x86_64.whl --prefix=/home/marcelo/packages/numpy/1.16.2"
 
 def commands():
-    env.LD_LIBRARY_PATH.prepend("{root}/lib")
+    env.LD_LIBRARY_PATH.prepend("{root}/lib64")
+    env.PYTHONPATH.append("{root}/lib64/python2.7/site-packages/numpy")
     env.NUMPY_ROOT = '{root}'
 
 uuid = "repository.numpy"
