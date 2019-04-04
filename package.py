aad45b79bed58b7bf025023e68ef4ff06b9122e8
Marcelo Sercheli <msercheli@gmail.com>
2019-04-03 23:36:10 -0400

    small fix in the rez-env command to create environment

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ build_command = "pip install /home/marcelo/Dev/payload/numpy/numpy-1.16.2-cp27-c
 
 def commands():
     env.LD_LIBRARY_PATH.prepend("{root}/lib64")
-    env.PYTHONPATH.append("{root}/lib64/python2.7/site-packages/numpy")
+    env.PYTHONPATH.append("{root}/lib64/python2.7/site-packages")
     env.NUMPY_ROOT = '{root}'
 
 uuid = "repository.numpy"
